8bfffab05ed4929776c38fef030edf461843750e
Sterling <sterlingmd17@gmail.com>
2017-12-07 14:19:14 -0800

    Took footer from index put in to base.html. Added login button to base

--- janellewebsite/main.py
+++ janellewebsite/main.py
@@ -12,6 +12,15 @@ def index():
 
     return render_template("index.html", a=a, recipes=recipes)
 
+@app.route('/recipe', methods=['GET'])
+def recipe():
+
+    recipeid=int(request.args.get("id"))
+    arecipe= Recipe.query.filter_by(id=recipeid)
+
+    return render_template("recipe display.html", recipe=arecipe)
+
+
 
 if __name__ == '__main__':
     app.run()
