4abd03b5319a46000c01c71871aa51c7200d0168
sterling <sterlingmd17@gmail.com>
2018-04-10 11:46:12 -0700

    Got rid of app.py integrated into models.py

--- janellewebsite/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://nelly:nelly@localhost:3306/nelly'
-app.config['SQLALCHEMY_ECHO'] = True
-app.secret_key= "nelly"
